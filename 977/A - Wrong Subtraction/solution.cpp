c45a27a06740b1cd9575be1ba7b8baed3db37adf
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:27:54 +0600

    Add Wrong Subtraction [A] from Codeforces

---
+++ 977/A - Wrong Subtraction/solution.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n,k;
+    cin >> n >> k;
+    int i,j;
+    i=0;
+    while(i<k){
+        if(n%10==0){
+            n=n/10;
+            i++;
+        }
+        else{
+            n=n-1;
+            i++;}
+    }
+    cout << n;
+    return 0;
+}
