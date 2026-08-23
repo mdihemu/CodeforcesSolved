e1cc3e52d2fcb31b877c9055f2d5765269bea1c9
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:52:27 +0600

    Add Floor Number [A] from Codeforces

---
+++ 1426/A - Floor Number/solution.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    int _=1;
+    cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n,x;
+    cin >> n >> x;
+    int cnt=0;
+    if(n<=2){
+        cnt = 1;
+    }
+    else{
+        n-=2;
+        cnt = (n+x-1)/x + 1;
+    }
+    cout << cnt << endl;
+}
