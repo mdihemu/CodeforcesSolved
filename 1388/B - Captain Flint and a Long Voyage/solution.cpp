d779fd04d7a61926c73d2bcfc2393e450912252c
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:47:16 +0600

    Add Captain Flint and a Long Voyage [B] from Codeforces

---
+++ 1388/B - Captain Flint and a Long Voyage/solution.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    int _=1; cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    string s = "";
+ 
+    int e = (n+3)/4;
+    for(int i=e;i<n;i++){
+        s += '9';
+    }
+    for(int i=0;i<e;i++){
+        s += '8';
+    }
+ 
+    cout << s << endl;
+}
