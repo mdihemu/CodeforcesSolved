deec6abb4a8cfeb0e27423c2818f893f4c006ada
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:30:48 +0600

    Add Level Statistics [A] from Codeforces

---
+++ 1334/A - Level Statistics/solution.cpp
@@ -0,0 +1,36 @@
+/*
+Author: DarkMotion
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int p=0,q=0,cnt=0;
+        while(n--){
+            int x,y;
+            cin >> x >> y;
+ 
+            if( x < p || y < q || x-p < y-q)
+                cnt++;
+ 
+            p = x;
+            q = y;
+        }
+        if(cnt ==0)
+            cout << "YES
+";
+        else
+            cout << "NO
+";
+    }
+}
+ 
