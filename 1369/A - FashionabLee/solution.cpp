10ee43b59e3dc016092e743904e1a736d610aefe
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:38:25 +0600

    Add FashionabLee [A] from Codeforces

---
+++ 1369/A - FashionabLee/solution.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define pb push_back
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IN freopen("input.txt","r",stdin);
+#define OUT freopen("output.txt","w",stdout);
+ 
+const int N = 2e5 + 5;
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    int t; cin >> t;
+    while(t--) solve();
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    if(n%4==0)
+        cout << "YES
+";
+    else
+        cout << "NO
+";
+}
+ 
