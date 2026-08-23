c99b8c67fde40399582a3eca6a8f8c9e32171884
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:41:37 +0600

    Add Make It Good [C] from Codeforces

---
+++ 1385/C - Make It Good/solution.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    int t=1; cin >> t;
+    while(t--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+    int cnt = n-1;
+    while(cnt>0){
+        if(a[cnt-1]>=a[cnt])
+            cnt--;
+        else
+            break;
+    }
+    while(cnt>0){
+        if(a[cnt-1]<=a[cnt])
+            cnt--;
+        else
+            break;
+    }
+    cout << cnt << endl;
+}
