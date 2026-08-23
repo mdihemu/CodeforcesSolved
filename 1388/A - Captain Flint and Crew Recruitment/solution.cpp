214c1a4eb39e759132d7b987726bdc0a83bca4e9
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:47:12 +0600

    Add Captain Flint and Crew Recruitment [A] from Codeforces

---
+++ 1388/A - Captain Flint and Crew Recruitment/solution.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    int _=1; cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    if(n<=30)
+        cout << "NO
+";
+    else{
+        cout << "YES
+";
+        int a = 6,b=10,c=14;
+        int d = n-30;
+        if(a==d || b==d || c==d ){
+            c++;
+            d--;
+        }
+        cout << a << " " << b << " " << c << " " << d << endl;
+    }
+}
