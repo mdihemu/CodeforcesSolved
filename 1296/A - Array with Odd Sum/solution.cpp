947ac00f22681dd903f5e108973262cbfa6613d8
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:29:43 +0600

    Add Array with Odd Sum [A] from Codeforces

---
+++ 1296/A - Array with Odd Sum/solution.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int n;
+    cin >> n;
+    while(n--){
+        int a;
+        cin >> a;
+        int coun = 0;
+        bool odd = false, even = false;
+        for(int i=0;i<a;i++){
+            int m;
+            cin >> m;
+            coun = coun +m;
+            odd |= m%2!=0;
+            even |= m%2==0;
+        }
+        if(coun %2 !=0 || (odd && even))
+            cout << "YES
+";
+        else
+            cout << "NO
+";
+    }
+    return 0;
+}
