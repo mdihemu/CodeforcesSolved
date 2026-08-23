02ab6293d52c4a7e5e7efab317f1390f94addfb2
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:42:55 +0600

    Add Dice Tower [B] from Codeforces

---
+++ 1266/B - Dice Tower/solution.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    int _=1; cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    if(n%14>=1 && n%14<=6 && n>14)
+        cout << "YES
+";
+    else
+        cout << "NO
+";
+}
