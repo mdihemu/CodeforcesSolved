4773d5eb9c3e714a0661907e2d4f4614194c23d6
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:43:33 +0600

    Add Repeating Cipher [A] from Codeforces

---
+++ 1095/A - Repeating Cipher/solution.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    int n;
+    cin >> n;
+    string s,str="";
+    cin >> s;
+    int cnt=1;
+    for(int i=0;i<n;i++){
+        str += s[i];
+        i+=cnt;
+        cnt++;
+    }
+    cout << str << endl;
+}
