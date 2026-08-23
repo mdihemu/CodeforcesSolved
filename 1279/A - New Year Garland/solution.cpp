a0fbf66d6606949ca0ef256d730db0534d395a25
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:41:53 +0600

    Add New Year Garland [A] from Codeforces

---
+++ 1279/A - New Year Garland/solution.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    int _=1; cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int a[3];
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a,a+3);
+    if(a[2] <= a[0]+a[1]+1)
+        cout << "Yes
+";
+    else
+        cout << "No
+";
+}
