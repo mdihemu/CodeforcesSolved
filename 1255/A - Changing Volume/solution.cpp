c60decdeaab8889ab636cb0aa8fc8ba21dac4395
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:43:18 +0600

    Add Changing Volume [A] from Codeforces

---
+++ 1255/A - Changing Volume/solution.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    int _=1; cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int a,b;
+    cin >> a >> b;
+    int n = abs(a-b);
+    int cnt = 0;
+    cnt += n/5;
+    n %= 5;
+    cnt += n/2;
+    n %= 2;
+    cnt += n;
+    cout << cnt << endl;
+}
