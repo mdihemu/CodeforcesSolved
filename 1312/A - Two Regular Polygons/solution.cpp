472eed30fbfe7c8e7a242f8caed6654c785b6cc1
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:32:25 +0600

    Add Two Regular Polygons [A] from Codeforces

---
+++ 1312/A - Two Regular Polygons/solution.cpp
@@ -0,0 +1,25 @@
+/*
+Author: DarkMotion
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--){
+        int a,b;
+        cin >> a >> b;
+        if(a%b==0)
+            cout << "YES
+";
+        else
+            cout << "NO
+";
+    }
+}
+ 
