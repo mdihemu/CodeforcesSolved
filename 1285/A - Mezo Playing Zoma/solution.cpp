e1a44c2da5ed593b6a97ed165a6b45c3ecb96338
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:31:20 +0600

    Add Mezo Playing Zoma [A] from Codeforces

---
+++ 1285/A - Mezo Playing Zoma/solution.cpp
@@ -0,0 +1,17 @@
+/*
+Author: DarkMotion
+*/
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main()
+{
+    int n;
+    string s;
+    cin >> n >> s;
+    cout << n+1 << endl;
+}
+ 
