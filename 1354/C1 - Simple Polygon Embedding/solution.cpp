8300d29d69ad4e5b182a497fe629145e32e5780d
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:36:12 +0600

    Add Simple Polygon Embedding [C1] from Codeforces

---
+++ 1354/C1 - Simple Polygon Embedding/solution.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define pb push_back
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define IN freopen("input.txt","r",stdin);
+#define OUT freopen("output.txt","w",stdout);
+ 
+const int N = 2e5 + 5;
+const double pi = 3.1415926535897;
+ 
+void solve();
+void cf()
+{
+    int t; cin >> t; while(t--) solve();
+}
+int32_t main()
+{
+    IOS;
+    cf();
+    ///solve();
+}
+ 
+void solve()
+{
+    long double n;
+    cin >> n;
+    n = 2 * n;
+    n = pi / n;
+    n = tan(n);
+    n = 1.0 / n;
+    cout << fixed << setprecision(9);
+    cout << n << endl;
+}
