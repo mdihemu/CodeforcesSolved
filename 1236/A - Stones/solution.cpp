d92ab58ccf49c8518a5f46d6f559770c75032fad
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:43:26 +0600

    Add Stones [A] from Codeforces

---
+++ 1236/A - Stones/solution.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define int long long
+#define endl "
+"
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    int _=1; cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int a,b,c;
+    cin >> a >> b >> c;
+    int x = min(b,c/2);
+    int cnt = x*3;
+    b -=x;
+    x = min(a,b/2);
+    cnt+=x*3;
+    cout << cnt << endl;
+}
