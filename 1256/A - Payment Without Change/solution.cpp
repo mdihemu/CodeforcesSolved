87b6dc05382d26c9e2134c71f870dc108c5cc052
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-23 23:29:47 +0600

    Add Payment Without Change [A] from Codeforces

---
+++ 1256/A - Payment Without Change/solution.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    int q;
+    cin >> q;
+ 
+    for(int i=0;i<q;i++){
+        int a,b,n,s;
+        cin >> a >> b >> n >> s;
+ 
+        if(s%n <=b && (1ll * a * n + b >=s))
+            cout << "YES
+";
+        else
+            cout << "NO
+";
+    }
+}
