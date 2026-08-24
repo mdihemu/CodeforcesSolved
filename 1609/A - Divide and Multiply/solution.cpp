7f3f6e2ccc8bcca3a7abcd5a97e892bf4573616a
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-24 22:46:45 +0600

    Add Divide and Multiply [A] from Codeforces

---
+++ 1609/A - Divide and Multiply/solution.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define     IOS         ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     int         long long
+#define     endl        "
+"
+#define     PI          acos(-1.0)
+#define     IN          freopen("input.txt",'r',stdin)
+ 
+const int MOD = 1e9+7;
+const int INF = 2e5+5;
+const int N = 205;
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    cout << fixed << setprecision(10);
+    int _ = 1;
+    cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+    }
+ 
+    int lol = 0;
+    for(int i=0;i<n;i++){
+        while(a[i]>1){
+            if(a[i] % 2 == 0){
+                a[i] /= 2;
+                lol++;
+            }
+            else{
+                break;
+            }
+        }
+    }
+    sort(a, a+n);
+    while(lol--){
+        a[n-1] *= 2;
+    }
+    int cnt = 0;
+    for(int i=0;i<n;i++){
+        cnt += a[i];
+    }
+    cout << cnt << endl;
+}
+ 
+///Must see the constraints range
+///Calculate the Time
