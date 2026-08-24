c969eff84dc0d9aa8ae5469f7b360a1f2080244a
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-24 22:34:18 +0600

    Add Yet Another Palindrome Problem [B] from Codeforces

---
+++ 1324/B - Yet Another Palindrome Problem/solution.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin >> a[i];
+    bool ok = false;
+    for(int i=0;i<n;i++){
+        for(int j=i+2;j<n;j++){
+            if(a[i] == a[j]){
+                ok = true;
+                break;
+            }
+        }
+        if(ok)
+            break;
+    }
+ 
+    if(ok)
+        cout << "YES
+";
+    else
+        cout << "NO
+";
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+        solve();
+}
