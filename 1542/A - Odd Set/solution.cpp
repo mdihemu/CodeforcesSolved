daa391d230d9867505c9abea08e12df94e219895
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-24 22:40:22 +0600

    Add Odd Set [A] from Codeforces

---
+++ 1542/A - Odd Set/solution.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define     IOS         ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     int         long long
+#define     endl        "
+"
+#define     PI          acos(-1.0)
+#define     IN          freopen("input.txt",'r',stdin)
+ 
+const int MOD = 1e9+7;
+const int INF = 2e5+5;
+const int N = 205;
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    cout << fixed << setprecision(10);
+    int _ = 1;
+    cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    n = 2*n;
+    int even = 0, odd = 0;
+    while(n--){
+        int x;
+        cin >> x;
+        if(x%2)
+            odd++;
+        else
+            even++;
+    }
+    if(even == odd)
+        cout << "YES
+";
+    else
+        cout << "NO
+";
+}
+ 
+///Must see the constraints range
+///Calculate the Time
