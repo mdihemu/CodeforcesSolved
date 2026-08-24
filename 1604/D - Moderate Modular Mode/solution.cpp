1d58bcbe06cf36e78c72c7c3727b9dca0334f53a
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-24 22:45:45 +0600

    Add Moderate Modular Mode [D] from Codeforces

---
+++ 1604/D - Moderate Modular Mode/solution.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define     IOS         ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     int         long long
+#define     endl        "
+"
+#define     PI          acos(-1.0)
+#define     IN          freopen("input.txt",'r',stdin)
+ 
+const int MOD = 1e9+7;
+const int INF = 2e5+5;
+const int N = 205;
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    cout << fixed << setprecision(10);
+    int _ = 1;
+    cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+ 
+    if(b%a == 0){
+        cout << a << endl;
+        return;
+    }
+    if(a%b == 0){
+        cout << (a*b)+b << endl;
+        return;
+    }
+    if(b>a){
+        a *= (b/a);
+        cout << (a+b)/2 << endl;
+        return;
+    }
+    cout << a + b << endl;
+}
+ 
+///Must see the constraints range
+///Calculate the Time
