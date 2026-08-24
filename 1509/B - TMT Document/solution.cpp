8b1df321ea0ecf04fd1b83244557310f8d4c10e7
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-24 22:37:17 +0600

    Add TMT Document [B] from Codeforces

---
+++ 1509/B - TMT Document/solution.cpp
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define     IOS         ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     int         long long
+#define     endl        "
+"
+#define     PI          acos(-1.0)
+#define     IN          freopen("input.txt",'r',stdin)
+ 
+const int MOD = 1e9+7;
+const int INF = 2e5+5;
+const int N = 205;
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    cout << fixed << setprecision(10);
+    int _ = 1;
+    cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int cnt = n/3;
+    int cnts = count(s.begin(), s.end(),'M');
+    if(cnt != cnts){
+        cout << "NO
+";
+        return;
+    }
+    if(s[0]=='M' || s[n-1]=='M'){
+        cout << "NO
+";
+        return;
+    }
+ 
+    int ft = 0, fm = 0;
+    for(int i=0;i<n;i++){
+        if(s[i] == 'T')
+            ft++;
+        else
+            fm++;
+        if(fm>ft){
+            cout << "NO
+";
+            return;
+        }
+    }
+ 
+    reverse(s.begin(), s.end());
+    ft = 0,fm = 0;
+    for(int i=0;i<n;i++){
+        if(s[i] == 'T')
+            ft++;
+        else
+            fm++;
+        if(fm>ft){
+            cout << "NO
+";
+            return;
+        }
+    }
+ 
+    cout << "YES
+";
+}
+ 
+///Must see the constraints range
+///Calculate the Time
