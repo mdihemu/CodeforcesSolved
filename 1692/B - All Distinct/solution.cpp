9b174ee096b455e5beee352904a494508bb8181f
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-24 22:49:13 +0600

    Add All Distinct [B] from Codeforces

---
+++ 1692/B - All Distinct/solution.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     int              long long
+#define     endl            "
+"
+#define     PI               acos(-1.0)
+#define     IN              freopen("input.txt",'r',stdin)
+ 
+const int MOD = 1e9+7;
+const int INF = 2e5+5;
+const int N = 205;
+ 
+int BIGMOD(int b, int p)
+{
+    int ans = 1 % MOD, x = b % MOD;
+    while(p){
+        if(p&1)ans = (ans * x)%MOD;
+        x = (x*x)%MOD;
+        p >>= 1LL;
+    }
+    return ans;
+}
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    cout << fixed << setprecision(10);
+    int _ = 1;
+    cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    map<int, int>mp;
+    for(int i=0;i<n;i++){
+        cin >> a[i];
+        mp[a[i]]++;
+    }
+    int cnt = mp.size();
+    int dif = n - cnt;
+    if(dif%2){
+        cnt--;
+    }
+    cout << cnt << endl;
+}
+ 
+///Must see the constraints range
+///Calculate the Time
