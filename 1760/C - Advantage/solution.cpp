2f021e3f82d447114a73956393f3b6992fd306c9
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-24 22:51:34 +0600

    Add Advantage [C] from Codeforces

---
+++ 1760/C - Advantage/solution.cpp
@@ -0,0 +1,76 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     int             long long
+#define     endl            "
+"
+#define     PI              acos(-1.0)
+#define     IN              freopen("input.txt",'r',stdin)
+ 
+const int MOD = 1e9+7;
+const int INF = 2e5+5;
+const int N = 205;
+ 
+int BIGMOD(int b, int p)
+{
+    int ans = 1 % MOD, x = b % MOD;
+    while(p){
+        if(p&1)ans = (ans * x)%MOD;
+        x = (x*x)%MOD;
+        p >>= 1LL;
+    }
+    return ans;
+}
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    cout << fixed << setprecision(10);
+    int _ = 1;
+    cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int>v, vv, ans(n);
+    for(int i=0;i<n;i++){
+        int x;
+        cin >> x;
+        v.push_back(x);
+        vv.push_back(x);
+    }
+    /*
+    for(int i=0;i<n;i++){
+        cout << vv[i]  << ' ';
+    }
+    */
+    sort(v.rbegin(), v.rend());
+    for(int i=0;i<n;i++){
+        vv[i] = vv[i] - v[0];
+    }
+    /*
+    for(int i=0;i<n;i++){
+        cout << vv[i]  << ' ';
+    }
+    cout << endl;
+    */
+    for(int i=0;i<n;i++){
+        if(vv[i] == 0){
+            vv[i] = v[0] - v[1];
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout << vv[i]  << ' ';
+    }
+    cout << endl;
+}
+ 
+///Must see the constraints range
+///Calculate the Time
+ 
