92bca3d90adfd7c2a23ab814dd5d1a7ab1b36553
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-24 22:48:20 +0600

    Add X-Sum [D] from Codeforces

---
+++ 1676/D - X-Sum/solution.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     int              long long
+#define     endl            "
+"
+#define     PI               acos(-1.0)
+#define     IN              freopen("input.txt",'r',stdin)
+ 
+const int MOD = 1e9+7;
+const int INF = 2e5+5;
+const int N = 205;
+ 
+int BIGMOD(int b, int p)
+{
+    int ans = 1 % MOD, x = b % MOD;
+    while(p){
+        if(p&1)ans = (ans * x)%MOD;
+        x = (x*x)%MOD;
+        p >>= 1LL;
+    }
+    return ans;
+}
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    cout << fixed << setprecision(10);
+    int _ = 1;
+    cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    int a[n][m];
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            cin >> a[i][j];
+        }
+    }
+    int mx = 0;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            int cnt =  a[i][j];
+            for(int ii=i-1,jj=j-1;ii>=0 && jj>=0;ii--,jj--){
+                cnt += a[ii][jj];
+            }
+            for(int ii=i+1,jj=j+1;ii<n && jj<m;ii++,jj++){
+                cnt += a[ii][jj];
+            }
+            for(int ii=i-1,jj=j+1;ii>=0 && jj<m;ii--,jj++){
+                cnt += a[ii][jj];
+            }
+            for(int ii=i+1,jj=j-1;ii<n && jj>=0;ii++,jj--){
+                cnt += a[ii][jj];
+            }
+            mx = max(mx, cnt);
+        }
+    }
+    cout << mx << endl;
+}
+ 
+///Must see the constraints range
+///Calculate the Time
