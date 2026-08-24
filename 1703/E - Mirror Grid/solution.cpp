887ac14124f09caa27e52ab6169c92fc257547f9
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-24 22:49:51 +0600

    Add Mirror Grid [E] from Codeforces

---
+++ 1703/E - Mirror Grid/solution.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define     IOS             ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     int              long long
+#define     endl            "
+"
+#define     PI               acos(-1.0)
+#define     IN              freopen("input.txt",'r',stdin)
+ 
+const int MOD = 1e9+7;
+const int INF = 2e5+5;
+const int N = 205;
+ 
+int BIGMOD(int b, int p)
+{
+    int ans = 1 % MOD, x = b % MOD;
+    while(p){
+        if(p&1)ans = (ans * x)%MOD;
+        x = (x*x)%MOD;
+        p >>= 1LL;
+    }
+    return ans;
+}
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    cout << fixed << setprecision(10);
+    int _ = 1;
+    cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<string>v(n);
+    for(auto& s : v){
+        cin >> s;
+    }
+ 
+    int cnt = 0;
+    for(int i=0;i<n/2;i++){
+        for(int j=i;j<n-i;j++){
+            int temp = (v[i][j] == '1');
+            v[i][j] = '2';
+            temp += (v[n-1-i][n-1-j] == '1');
+            v[n-1-i][n-1-j] = '2';
+            temp += (v[n-1-j][i] == '1');
+            v[n-1-j][i] = '2';
+            temp += (v[j][n-1-i] == '1');
+            v[j][n-1-i] = '2';
+ 
+            cnt += min(temp, 4-temp);
+        }
+    }
+    cout << cnt << endl;
+}
+ 
+///Must see the constraints range
+///Calculate the Time
