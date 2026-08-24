b480a840be593671ebe1e9dd9cf1a4e2063065c4
Md. Imran Hossain Emu <56190551+mdihemu@users.noreply.github.com>
2026-08-24 22:42:03 +0600

    Add Polycarp and Coins [A] from Codeforces

---
+++ 1551/A - Polycarp and Coins/solution.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+#define     IOS         ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define     int         long long
+#define     endl        "
+"
+#define     PI          acos(-1.0)
+#define     IN          freopen("input.txt",'r',stdin)
+ 
+const int MOD = 1e9+7;
+const int INF = 2e5+5;
+const int N = 205;
+ 
+void solve();
+int32_t main()
+{
+    IOS;
+    cout << fixed << setprecision(10);
+    int _ = 1;
+    cin >> _;
+    while(_--) solve();
+    return 0;
+}
+ 
+void solve()
+{
+    int n;
+    cin >> n;
+    int a = n/3;
+    if(n%3==0)
+        cout << a << " " << a << endl;
+    else if(n%3==2)
+        cout << a << " " << a+1 << endl;
+    else
+        cout << a+1 << " " << a << endl;
+}
+ 
+///Must see the constraints range
+///Calculate the Time
